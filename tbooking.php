e46b43526a2502d2953dcd5962205d0f85b4929c
mca503@rajagiri.edu <mca503@rajagiri.edu>
2021-03-16 23:00:28 +0530

    m

--- tbooking.php
+++ tbooking.php
@@ -18,24 +18,24 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="C:\xampp\xampp\htdocs\Alvin\index.php">Home</a>
+          <a class="navbar-brand" href="index.php">Home</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="C:\xampp\xampp\htdocs\Alvin\tcontact.php">Contact</a>
+                <a class="nav-link active" aria-current="page" href="tcontact.php">Contact</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tgallery.php">Gallery</a>
+                <a class="nav-link" href="tgallery.php">Gallery</a>
               </li>
              
               <li class="nav-item">
-                <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tbooking.php">Bookings</a>
+                <a class="nav-link" href="tbooking.php">Bookings</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tcovid.php">Acknowledgement</a>
+                <a class="nav-link" href="tcovid.php">Acknowledgement</a>
               </li>
             </ul>
             <form class="d-flex">
