72fe46558d86f6cb77ef5132b7522a2e80ff5d7b
mca503@rajagiri.edu <mca503@rajagiri.edu>
2021-03-16 22:43:20 +0530

    Work

---
+++ tcontact.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="C:\xampp\xampp\htdocs\Alvin\index.php">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="C:\xampp\xampp\htdocs\Alvin\tcontact.php">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tgallery.php">Gallery</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tbooking.php">Bookings</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="C:\xampp\xampp\htdocs\Alvin\tcovid.php">Acknowledgement</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  
+        
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+   
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://images.thrillophilia.com/image/upload/s--A6FmIrCs--/c_fill,h_775,q_auto,w_1600/f_auto,fl_strip_profile/v1/images/photos/000/091/829/original/1466695382_5581982372111720778.jpg.jpg?1466695382" class="d-block w-100" alt="...">
+          </div>
+          
+        </div>
+      </div>
+           <div class="container">
+          <div class="row">
+              <div class="col col-12 col-sm-4 col-md-4">
+                  Office:0985683490
+              </div>
+              <div class="col col-12 col-sm-4 col-md-4">Naveen:9634081278</div>
+          
+          <div class="col col-12 col-sm-4 col-md-4">Sajan:8529705694</div> 
+      </div>
+    </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+</html>
